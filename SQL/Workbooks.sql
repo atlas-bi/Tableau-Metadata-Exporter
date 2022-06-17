d86121e0c28ce40a33691295901a1097b6906d18
Christopher Pickering <cpickering@rhc.net>
2022-06-17 12:48:02 -0500

    feat(build): updated to latest version, changed to github first

--- SQL/Workbooks.sql
+++ SQL/Workbooks.sql
@@ -1,13 +1,14 @@
-select distinct
-  workbooks.id,
+select
+  distinct workbooks.id,
   workbooks.name,
   workbooks.description,
   workbooks.created_at AT TIME ZONE 'UTC' AT TIME ZONE 'US/Central',
   workbooks.updated_at AT TIME ZONE 'UTC' AT TIME ZONE 'US/Central',
-  'https://tableau/#/views/' || workbooks.repository_url || '/' || views.sheet_id,
+  'TABLEAUURL/#/views/' || workbooks.repository_url || '/' || views.sheet_id,
   workbooks.repository_url,
   workbooks.owner_id
 from
   workbooks
   left outer join views on workbooks.id = views.workbook_id
-where views.index=1
+where
+  views.index = 1
